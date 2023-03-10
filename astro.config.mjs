7ff2972e6701910c205631e067c5f89b0e89b1ac
Anurag Roy <anuragroy@duck.com>
2023-03-11 02:40:53 +0530

    Add Vercel SSR config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/edge';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
 });
